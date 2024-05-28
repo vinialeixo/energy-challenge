8114ae2ddd51873f32f45273ba8fa2a10fe3799a
Vinicius <vi_aleixodias@hotmail.com>
2024-05-27 22:24:26 -0300

    fix: integration

--- api-energy/src/server.js
+++ api-energy/src/server.js
@@ -1,19 +1,13 @@
 import controller from './routes/controller.js'
 import express from 'express'
 import fileUpload from 'express-fileupload'
-// import bodyParser from 'body-parser'
-import cors from 'cors'
-// import dotenv from 'dotenv';
+
 
 const app = express();
 
 // Middleware para fazer o upload dos arquivos
 app.use(fileUpload());
 
-app.use(cors({
-	origin: '*',
-	methods: ['GET','POST','DELETE','PUT'],
-  }));
 
   app.use('/invoice',controller)
 
